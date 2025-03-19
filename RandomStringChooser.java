da0af363c5cb9eecd48195b03ad910cc7511d23b
haydenhobbbs <haydenrhobbs@gmail.com>
2025-03-19 10:07:05 -0700

    Update RandomStringChooser.java

--- RandomStringChooser.java
+++ RandomStringChooser.java
@@ -1,8 +1,21 @@
 package P5E_Project_RandomStringChooser_Template;
-
+private ArrayList<String>();
 public class RandomStringChooser
    {
+   public RandomStringChooser(String [] wordArray){
+           words = new ArrayList<String>();
+           
+           for(String singleWord : wordArray){
+               words.add(singleWord);
+           }
+       }
        
+       public String getNext(){
+           if(words.size() > 0){
+               return words.remove((int)(Math.random()*words.size()));
+           }
+           return "None";
+       }    
    public String toString()
       {
       String output = new String();
